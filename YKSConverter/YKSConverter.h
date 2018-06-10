61ab453640fe989491d46f7ed0eb273acdb24ba4
Chanwoo Noh <rajephon@gmail.com>
2018-06-10 18:43:22 +0900

    added comments

--- YKSConverter/YKSConverter.h
+++ YKSConverter/YKSConverter.h
@@ -13,16 +13,17 @@
 
 class YKSConverter : public std::enable_shared_from_this<YKSConverter> {
 public:
-    
     YKSConverter(const std::string &mml = "") {
         _mml = mml;
-        _timebase = 480;
         _timebase = 96;
     }
     
     void mml(const std::string &mml) { _mml = mml; }
     std::string mml() { return _mml; }
-    
+    /*
+     * MML string을 midi buffer로 변환
+     * return std::shared_ptr<YKS::ByteBuffer> 변환된 버퍼. 변환 실패 시 nullptr
+     */
     std::shared_ptr<YKS::ByteBuffer> toBuffer();
 private:
     
